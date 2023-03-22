490b4ea3ad8231d4fdbc6e036139254f074872cc
JasminKrnic <127773302+JasminKrnic@users.noreply.github.com>
2023-03-22 13:43:37 +0100

    changed array

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
 let pokemonList = [];
-pokemonList[0] = {
-  name: "Bulbasaur",
-  height: 0.7,
-  types: ["grass", "poison"],
-};
-
-pokemonList[1] = {
-  name: "Charmander",
-  height: 0.6,
-  types: ["fire", "speed"],
-};
-pokemonList[2] = {
-  name: "Squirtle",
-  height: 0.5,
-  types: ["water", "speed"],
-};
+pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Charmander",
+    height: 0.7,
+    types: ["fire", "speed"],
+  },
+  {
+    name: "Squirtle",
+    height: 0.5,
+    types: ["water", "speed"],
+  },
+];
