bcb446a2022a0c262fdcb35ca2f838a3bc4928e8
JasminKrnic <127773302+JasminKrnic@users.noreply.github.com>
2023-03-24 10:23:36 +0100

    applied IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,52 @@ pokemonList.forEach(function (name) {
       "<br>"
   );
 });
+
+//IIFE applied
+(function () {
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Charmander",
+      height: 1.7,
+      types: ["fire", "speed"],
+    },
+    {
+      name: "Squirtle",
+      height: 0.5,
+      types: ["water", "speed"],
+    },
+  ];
+})();
+//assigning IIFE to variable
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Charmander",
+      height: 1.7,
+      types: ["fire", "speed"],
+    },
+    {
+      name: "Squirtle",
+      height: 0.5,
+      types: ["water", "speed"],
+    },
+  ];
+  return {
+    add: function (pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function () {
+      return pokemonList;
+    },
+  };
+})();
